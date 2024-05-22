7e2502ca15cf2c8bab225d7392f850885ec5c538
davi siqueira <davisiqueirat@gmail.com>
2024-05-22 16:45:56 -0300

    adicionando meta tag og

--- script.js
+++ script.js
@@ -1,83 +1,74 @@
-const menu = document.getElementById("menu")
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("cart-modal")
-const cartItemsContainer = document.getElementById("cart-items")
-const cartTotal = document.getElementById("cart-total")
-const checkoutBtn = document.getElementById("checkout-btn")
-const closeModalBtn = document.getElementById("close-modal-btn")
-const cartCounter = document.getElementById("cart-count")
-const adressInput = document.getElementById ("address")
-const adressWarn = document.getElementById ("address-warn")
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const adressInput = document.getElementById("address");
+const adressWarn = document.getElementById("address-warn");
 
 let cart = [];
 
+cartBtn.addEventListener("click", function () {
+  updateCartModal();
+  cartModal.style.display = "flex";
+});
 
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
 
+closeModalBtn.addEventListener("click", function () {
+  cartModal.style.display = "none";
+});
 
-cartBtn.addEventListener("click", function(){
-    updateCartModal();
-    cartModal.style.display ="flex"
-})
-
-cartModal.addEventListener("click", function(event){
-   if(event.target === cartModal){
-    cartModal.style.display = "none"
-   }
-})
-
-closeModalBtn.addEventListener("click", function(){
-    cartModal.style.display = "none"
-})
-
-menu.addEventListener("click", function (event){
+menu.addEventListener("click", function (event) {
   //  console.log(event.target)
 
-  let parentButton = event.target.closest(".add-to-cart-btn")
- 
-  if(parentButton){
-    const name = parentButton.getAttribute("data-name")
-    const price = parseFloat(parentButton.getAttribute("data-price"))
+  let parentButton = event.target.closest(".add-to-cart-btn");
 
-    addToCart(name, price)
- }
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
 
-})
+    addToCart(name, price);
+  }
+});
 
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
 
-function addToCart (name, price){
-  const existingItem = cart.find(item => item.name === name)
-
-  if(existingItem){
+  if (existingItem) {
     existingItem.quantity += 1;
-  
-    }else{
+  } else {
     cart.push({
-        name,
-        price,
-        quantity: 1,
-    })
-  
-}
-   
- updateCartModal()
-    
+      name,
+      price,
+      quantity: 1,
+    });
+  }
 
+  updateCartModal();
 }
 
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
 
+  cart.forEach((item) => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add(
+      "flex",
+      "justify-between",
+      "mb-4",
+      "flex-col"
+    );
 
-
-function updateCartModal(){
-    cartItemsContainer.innerHTML = "";
-    let total = 0;
-
-
-   cart.forEach(item => {
-   const cartItemElement = document.createElement("div");
-    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
-
-
-   cartItemElement.innerHTML = `
+    cartItemElement.innerHTML = `
    <div class = "flex items-center justify-between ">   
     <div>
     <p class = "font-bold">${item.name}</p> 
@@ -94,115 +85,108 @@ function updateCartModal(){
 
     </div>
 
-   `
-   total += item.price * item.quantity;
+   `;
+    total += item.price * item.quantity;
 
-   cartItemsContainer.appendChild(cartItemElement)
-   }) 
+    cartItemsContainer.appendChild(cartItemElement);
+  });
 
-   cartTotal.textContent = total.toLocaleString("pt-BR", {
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
     style: "currency",
-    currency: "BRL"
-   })
-    
-   cartCounter.innerHTML = cart.length;
+    currency: "BRL",
+  });
 
-
-
+  cartCounter.innerHTML = cart.length;
 }
 
+cartItemsContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name");
 
-cartItemsContainer.addEventListener("click", function (event){
-    if(event.target.classList.contains("remove-from-cart-btn")){
-        const name = event .target.getAttribute("data-name")
+    removeItemCart(name);
+  }
+});
 
-        removeItemCart(name);      
-    }
-})
+function removeItemCart(name) {
+  const index = cart.findIndex((item) => item.name === name);
 
-function removeItemCart(name){
-    const index = cart.findIndex(item => item.name === name);
-
-    if(index !== -1){
+  if (index !== -1) {
     const item = cart[index];
-  
-    if(item.quantity > 1){
-        item.quantity -= 1;
-        updateCartModal();
-        return;
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
     }
-    
+
     cart.splice(index, 1);
     updateCartModal();
-
+  }
 }
 
+adressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    adressInput.classList.remove("border-red-500");
+    adressWarn.classList.add("hidden");
+  }
+});
+
+checkoutBtn.addEventListener("click", function () {
+  const isOpen = checkRestaurantOpen();
+  if (!isOpen) {
+    Toastify({
+      text: "Ops o restaurante esta fechado",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#ef4444",
+      },
+    }).showToast();
+
+    return;
+  }
+
+  if (cart.lenght === 0) return;
+  if (adressInput.value === "") {
+    adressWarn.classList.remove("hidden");
+    adressInput.classList.add("border-red-500");
+    return;
+  }
+
+  const cartItems = cart
+    .map((item) => {
+      return `${item.name} quantidade: (${item.quantity}) Preco: R$${item.price} |`;
+    })
+    .join("");
+
+  const message = encodeURIComponent(cartItems);
+  const phone = "5581999899083";
+  window.open(
+    `https://wa.me/${phone}?text=${message} endereco: ${adressInput.value}`,
+    "_blank"
+  );
+
+  cart = [];
+  updateCartModal();
+});
+
+function checkRestaurantOpen() {
+  const data = new Date();
+  const hora = data.getHours();
+  return hora >= 18 && hora < 22;
 }
-
-adressInput.addEventListener("input", function(event){
-    let inputValue = event.target.value;
-
-    if(inputValue !== ""){
-        adressInput.classList.remove("border-red-500")
-        adressWarn.classList.add("hidden")
-    }
-
-})
-
-checkoutBtn.addEventListener("click", function(){
-
-     const isOpen = checkRestaurantOpen();
-     if(!isOpen){
-         Toastify({
-            text: "Ops o restaurante esta fechado",
-            duration: 3000,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "#ef4444",
-            },
-            }).showToast();
-         
-            return;
-     }
-
-    if(cart.lenght ===0) return;
-  if(adressInput.value === ""){
-        adressWarn.classList.remove("hidden")
-        adressInput.classList.add("border-red-500")
-        return;
-    }
-
-
-    const cartItems = cart.map((item) => {
-        return(
-            `${item.name} quantidade: (${item.quantity}) Preco: R$${item.price} |`
-        )
-    }).join("")
-
-   const message = encodeURIComponent(cartItems)
-   const phone = "5581999899083"
-   window.open(`https://wa.me/${phone}?text=${message} endereco: ${adressInput.value}`, "_blank")
-
-   cart = [];
-   updateCartModal();
-})
-
-function checkRestaurantOpen(){
-    const data = new Date();
-    const hora = data.getHours();
-    return hora >= 18 && hora < 22;
-}
-const spanItem = document.getElementById("date-span")
+const spanItem = document.getElementById("date-span");
 const isOpen = checkRestaurantOpen();
 
-
-if(isOpen){
-    spanItem.classList.remove("bg-red-500");
-    spanItem.classList.add("bg-green-600")
-    }else{
-        spanItem.classList.remove("bg-green-600")
-        spanItem.classList.add("bg-red-500")
-    }
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else {
+  spanItem.classList.remove("bg-green-600");
+  spanItem.classList.add("bg-red-500");
+}
